4f9bfb43ccbc89289dc2041d3b385b813ead8993
Facundo Guerrero <guerremdq@gmail.com>
2018-07-02 21:29:30 +0200

    change listen host

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -9,9 +9,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def get():
+    print(request.headers)
     return "Nginx say: {}".format(requests.get('http://nginx.facu.svc.cluster.local').status_code)
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
